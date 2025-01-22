a1633affb3f4f22bc86f49e7001e039e132b1904
ayoubissaad <ayoub.issaad@gmail.com>
2025-01-22 20:28:52 +0100

    cat8

--- src/components/pages/CatPage.jsx
+++ src/components/pages/CatPage.jsx
@@ -204,7 +204,7 @@ export function CatPage() {
                   <video
                     ref={videoRef}
                     src="/cat.webm"
-                    className="w-[280px] h-[280px] object-cover"
+                    className="w-[170px] h-auto object-cover"
                     playsInline
                     loop
                     muted
